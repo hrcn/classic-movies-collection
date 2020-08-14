b9179444d776e749f81b279a08e69b5c4e1f394d
hrcn <hrcn1128@gmail.com>
2020-08-14 19:11:34 -0400

    added user model and nodemon

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const { User } = require('./models/user');
+
 mongoose.connect('mongodb+srv://hrcn:hanrui1996@cluster0.cua0b.mongodb.net/<dbname>?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -10,8 +15,24 @@ mongoose.connect('mongodb+srv://hrcn:hanrui1996@cluster0.cua0b.mongodb.net/<dbna
   console.error(err);
 })
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
-  res.send("Hello!");
+  res.json({"Hello!": "Hi!"});
+})
+
+// postman testing
+app.post('/api/users/register', (req, res) => {
+  const user = new User(req.body); // create a new user model
+
+  // save the user model
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err })
+
+    return res.status(200).json({ success: true });
+  }); 
 })
 
 app.listen(5000);
