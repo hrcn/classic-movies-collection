780335fbff0c885583fcbea3a3e5213c3f8b2771
hrcn <hrcn1128@gmail.com>
2020-09-04 16:21:17 -0400

    added movie DB API

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,7 @@
 // SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_KEY = 'ef30f4e9c750cffe15946a29e54f094e';
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_URL = 'https://image.tmdb.org/t/p/'
