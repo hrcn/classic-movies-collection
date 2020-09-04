2f17ac7500963dfb8efe1e712e4855ec2c46e515
hrcn <hrcn1128@gmail.com>
2020-09-04 16:16:37 -0400

    set up boilerplate

--- client/src/index.js
+++ client/src/index.js
@@ -1,35 +1,34 @@
+import 'core-js';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-
 import { BrowserRouter } from 'react-router-dom';
-
-// materilize css
-import "materialize-css/dist/css/materialize.min.css";
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
-
 import Reducer from './reducers';
+import * as serviceWorker from './serviceWorker';
+import App from './components/App';
 
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
-  // use redux devtools extension
-  <Provider store={createStoreWithMiddleware(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}> 
+  <Provider
+    store={createStoreWithMiddleware(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__
+            && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    )}
+  >
     <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <App />
     </BrowserRouter>
-  </Provider>
-  ,document.getElementById('root')
+  </Provider>,
+  document.getElementById('root'),
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
