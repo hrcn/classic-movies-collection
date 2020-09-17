b2c9a778d8b2b4fd377bd00efcda92cc93a289c9
hrcn <hrcn1128@gmail.com>
2020-09-17 16:40:46 -0400

    add and remove from favorites

--- client/src/components/views/MovieDetailPage/Favorite.js
+++ client/src/components/views/MovieDetailPage/Favorite.js
@@ -1,44 +1,75 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Button } from 'antd';
+import { response } from 'express';
 
 function Favorite(props) {
-  const [FavoriteNumber, setFavoriteNumber] = useState(0)
-  const [Favorited, setFavorited] = useState(false)
+  const [FavoriteNumber, setFavoriteNumber] = useState(0);
+  const [Favorited, setFavorited] = useState(false);
+
+  const variable = {
+    userFrom: props.userFrom,
+    movieId: props.movieId,
+    movieTitle: props.movieInfo.original_title,
+    movieImage: props.movieInfo.backdrop_path,
+    movieRunTime: props.movieInfo.runtime,
+  };
 
   useEffect(() => {
-    const variable = {
-      userFrom: props.userFrom,
-      movieId: props.movieId,
-      movieTitle: props.movieInfo.original_title,
-      movieImage: props.movieInfo.backdrop_path,
-      movieRunTime: props.movieInfo.runtime
-    }
-
     axios.post('/api/favorite/favoriteNumber', variable)
-      .then(response => {
+      .then((response) => {
         if (response.data.success) {
-          setFavoriteNumber(response.data.favoriteNumber) 
+          setFavoriteNumber(response.data.favoriteNumber);
         } else {
-          alert("Failed to get favoriteNumber")
+          alert('Failed to get favoriteNumber');
         }
-      })
+      });
 
     axios.post('/api/favorite/favorited', variable)
-      .then(response => {
+      .then((response) => {
         if (response.data.success) {
-          setFavoriteNumber(response.data.favorited) 
+          setFavoriteNumber(response.data.favorited);
         } else {
-          alert("Failed to get favorite info")
+          alert('Failed to get favorite info');
         }
-      })
-  }, [])
+      });
+  }, []);
+
+  const onClickFavorite = () => {
+    if (Favorited) {
+      // already added
+      axios.post('./api/favorite/removeFromFavorite', variable)
+        .then((response) => {
+          if (response.data.success) {
+            setFavoriteNumber(FavoriteNumber - 1);
+            setFavorited(!Favorited);
+          } else {
+            alert('Failed to remove from favorites');
+          }
+        });
+    } else {
+      // not added
+      axios.post('./api/favorite/addToFavorite', variable)
+        .then((response) => {
+          if (response.data.success) {
+            setFavoriteNumber(FavoriteNumber + 1);
+            setFavorited(!Favorited);
+          } else {
+            alert('Failed to add to favorites');
+          }
+        });
+    }
+  };
 
   return (
     <div>
-      <Button>{Favorited ? "Remove From Favorite" : "Add To Favorite"} {FavoriteNumber}</Button>
+      <Button onClick={onClickFavorite}>
+        {Favorited ? 'Remove From Favorite' : 'Add To Favorite'}
+        {' '}
+        {FavoriteNumber}
+      </Button>
     </div>
-  )
+  );
 }
 
-export default Favorite
+export default Favorite;
--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -1,29 +1,47 @@
 const express = require('express');
+
 const router = express.Router();
 const { Favorite } = require('../models/Favorite');
 const { auth } = require('../middleware/auth');
 
 router.post('/favoriteNumber', auth, (req, res) => {
   // find favorite information inside Favorite Collection by movie ID
-  Favorite.find({"movieId": req.body.movieId})
+  Favorite.find({ movieId: req.body.movieId })
     .exec((err, favorite) => {
-      if(err) return res.status(400).send(err)
-      res.status(200).json({success: true, favoriteNumber: favorite.length})
-    })
+      if (err) return res.status(400).send(err);
+      res.status(200).json({ success: true, favoriteNumber: favorite.length });
+    });
 });
 
 router.post('/favorited', auth, (req, res) => {
   // find favorite information inside Favorite Collection by movie ID & userFrom
-  Favorite.find({"movieId": req.body.movieId, "userFrom": req.body.userFrom})
+  Favorite.find({ movieId: req.body.movieId, userFrom: req.body.userFrom })
     .exec((err, favorite) => {
-      if(err) return res.status(400).send(err)
+      if (err) return res.status(400).send(err);
       // already favorite a movie or not?
       let result = false;
       if (favorite.length !== 0) {
         result = true;
       }
-      res.status(200).json({success: true, favorite: result})
-    })
+      res.status(200).json({ success: true, favorite: result });
+    });
+});
+
+router.post('/addToFavorite', auth, (req, res) => {
+  // save the info about movie and user ID
+  const favorite = new Favorite(req.body);
+  favorite.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
+
+router.post('/removeFromFavorite', auth, (req, res) => {
+  Favorite.findOneAndDelete({ movieId: req.body.movieId, userFrom: req.body.userFrom })
+    .exec((err, doc) => {
+      if (err) return res.status(400).json({ success: false, err });
+      res.status(200).json({ success: true, doc });
+    });
 });
 
 module.exports = router;
